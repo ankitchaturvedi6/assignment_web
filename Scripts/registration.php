98c9f029e78ed0c1c58a63e857882b2165bb8808
root <hitesh.jivanani@bigsteptech.com>
2021-12-09 23:46:00 +0530

    Connection and registration file for signup

---
+++ Scripts/registration.php
@@ -0,0 +1,28 @@
+<?php
+include dirname(__FILE__).'/../Database/connection.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Something posted
+  
+    if (isset($_POST['registration'])) {
+        
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $email = $_POST['email'];
+        $mobile = $_POST['mobile'];
+        $password = $_POST['password'];
+        $confirm_password = $_POST['confirm_password'];
+        if($password===$confirm_password){
+            $sqlquery = "INSERT INTO users VALUES(null,'$firstname', '$lastname', '$mobile', '$mobile', '$password')";
+            if ($conn->query($sqlquery) === TRUE) {
+                header('Location: login');
+            }                                     
+        }
+       
+    } 
+  }
+
+
+
+
+?>
--- views/signup.php
+++ views/signup.php
@@ -1,3 +1,7 @@
+<?php   
+include dirname(__FILE__).'/../Scripts/registration.php';
+
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -11,33 +15,48 @@
             <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
                 <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
                     <h1 class="mb-8 text-3xl text-center">Sign up</h1>
+                    <form method="POST" action="">
                     <input 
                         type="text"
                         class="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="fullname"
-                        placeholder="Full Name" />
+                        name="firstname"
+                        placeholder="First Name" required/>
 
                     <input 
-                        type="text"
+                    type="text"
+                    class="block border border-grey-light w-full p-3 rounded mb-4"
+                    name="lastname"
+                    placeholder="Last Name" required/>
+
+                    <input 
+                    type="number"
+                    class="block border border-grey-light w-full p-3 rounded mb-4"
+                    name="mobile"
+                    placeholder="Mobile Number" required/>
+
+                    <input 
+                        type="email"
                         class="block border border-grey-light w-full p-3 rounded mb-4"
                         name="email"
-                        placeholder="Email" />
+                        placeholder="Email" required/>
 
                     <input 
                         type="password"
                         class="block border border-grey-light w-full p-3 rounded mb-4"
                         name="password"
-                        placeholder="Password" />
+                        placeholder="Password" required/>
                     <input 
                         type="password"
                         class="block border border-grey-light w-full p-3 rounded mb-4"
                         name="confirm_password"
-                        placeholder="Confirm Password" />
+                        placeholder="Confirm Password" required/>
 
-                    <button
+                    <input
                         type="submit"
+                        name="registration"
+                        value="Create Account"
                         class="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
-                    >Create Account</button>
+                    >
                 </div>
 
                 <div class="text-grey-dark mt-6">
