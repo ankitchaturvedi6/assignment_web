585daa78957e3bcc8ffb94caab7e33d5b33277b2
ankit-chaturvedi-bs <ankit.chaturvedi@bigsteptech.com>
2021-12-11 01:33:21 +0530

    request evaluation page creation

--- index.php
+++ index.php
@@ -14,6 +14,12 @@ switch($request) {
     case "/home":
         require __DIR__ . '/views/home.php';
         break;
+    case "/request_evaluation":
+        require __DIR__ . '/views/request_evaluation.php';
+        break;
+    case "/admin":
+        require __DIR__ . '/views/admin.php';
+        break;        
     default:
         http_response_code(404);
         require __DIR__ . '/views/signup.php';
---
+++ views/admin.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://unpkg.com/tailwindcss@2.0.3/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container grid grid-cols-3 gap-2 mx-auto">
+        <div class="w-32 h-32 rounded">
+            <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="image" class="w-24 h-24">
+        </div>
+        <div class="w-32 h-32 rounded">
+            <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="image" class="w-24 h-24">
+        </div>
+        <div class="w-32 h-32 rounded">
+            <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="image" class="w-24 h-24">
+        </div>
+        <div class="w-32 h-32 rounded">
+            <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="image" class="w-24 h-24">
+        </div>
+        <div class="w-32 h-32 rounded">
+            <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="image" class="w-24 h-24">
+        </div>
+        <div class="w-32 h-32 rounded">
+            <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+            alt="image" class="w-24 h-24">
+        </div>
+    </div>   
+</body>
+</html>l
