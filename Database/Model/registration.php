15508891423dda6d60606013666a9df8a0ef5aaf
root <hitesh.jivanani@bigsteptech.com>
2021-12-11 22:39:56 +0530

    signup and login with hash_pass

--- Database/Model/registration.php
+++ Database/Model/registration.php
@@ -1,28 +1,30 @@
 <?php
-include dirname(__FILE__).'/../connection.php';
-
+include dirname(__FILE__) . '/../connection.php';
+$email_error       = '';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
     // Something posted
-  
     if (isset($_POST['registration'])) {
         
-        $firstname = $_POST['firstname'];
-        $lastname = $_POST['lastname'];
-        $email = $_POST['email'];
-        $mobile = $_POST['mobile'];
-        $password = $_POST['password'];
-        $confirm_password = $_POST['confirm_password'];
-        if($password===$confirm_password){
-            $sqlquery = "INSERT INTO users VALUES(null,'$firstname', '$lastname', '$mobile', '$mobile', '$password')";
-            if ($conn->query($sqlquery) === TRUE) {
-                header('Location: login');
-            }                                     
+        $firstname         = $_POST['firstName'];
+        $lastname          = $_POST['lastName'];
+        $email             = $_POST['email'];
+        $mobile            = $_POST['mobile'];
+        $password          = $_POST['password'];
+        $confirm_password  = $_POST['confirm_password'];
+        //check pass is same of not
+        if ($password === $confirm_password) {
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+            $sql_e = "SELECT * FROM users WHERE email='$email'";
+            $res_e = mysqli_query($conn, $sql_e);
+            if (mysqli_num_rows($res_e) > 0) {
+                $email_error = "Sorry... email already taken";
+            } else {
+                $sqlquery = "INSERT INTO users VALUES(null,'$firstname', '$lastname', '$mobile', '$email', '$hashed_password')";
+                if ($conn->query($sqlquery) === TRUE) {
+                    header('Location: login');
+                }
+            }
         }
-       
-    } 
-  }
-
-
-
-
-?>
+    }
+}
